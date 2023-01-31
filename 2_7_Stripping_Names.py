419037a894369f30e74e44d8632024c12e915d18
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-01-30 18:36:49 -0600

    Add files via upload

---
+++ 2_7_Stripping_Names.py
@@ -0,0 +1,16 @@
+my_name = "\tRico Robinson\n"
+
+print("Regular:")
+print(my_name)
+
+print("\nUsing lstrip():")
+print(my_name.lstrip())
+
+print("\nUsing rstrip():")
+print(my_name.rstrip())
+
+
+print("\nUsing strip():")
+print(my_name.strip())
+
+# the first print statement for each answer shows the user which strip methods in using
---
+++ 2_9_favorite_number.py
@@ -0,0 +1,3 @@
+my_favortie_number = 15
+msg = f"My favorite number is {my_favortie_number}."
+print(msg)
