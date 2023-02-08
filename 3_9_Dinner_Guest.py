2d0d2369e8472a7a60e27733540d818a2edc1ada
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-02-08 16:49:24 -0600

    Add files via upload

---
+++ 3_9_Dinner_Guest.py
@@ -0,0 +1,14 @@
+guest = ["Crag", "Scooter", "Peewee"]
+
+names = guest[0].title()
+print(f"\n{names}, please come to dinner.")
+
+names = guest[1].title()
+print(f"\n{names}, please come to dinner.")
+
+names = guest[2].title()
+print(f"\n{names}, please come to dinner.")
+
+# we use len() to tell us how many people are coming to dinner
+number_guest = len(guest)
+print(f"\n{number_guest} people are coming to dinner")
