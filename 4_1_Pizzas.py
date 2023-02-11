bb587f373c477aede8a36cfde614a2478954beb5
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-02-11 11:51:25 -0600

    Add files via upload

---
+++ 4_1_Pizzas.py
@@ -0,0 +1,13 @@
+# My favorite pizzas 
+pizzas = ["cheese", "suasage", "peperoni"]
+
+# This is a "for loop"
+for pizza in pizzas:
+	print(pizza)
+
+# This is a "for loop to print out a statement"
+for pizza in pizzas:
+	print(f"\nI like {pizza} pizza!")
+
+
+print("\nI really love pizza!")
