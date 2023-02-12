a919b0fb472dca14ee7cbaceb89781078f41f1ed
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-02-12 09:55:14 -0600

    Add files via upload

---
+++ 4_2_Animals.py
@@ -0,0 +1,14 @@
+# animals that have something in common
+animals = ["lions", "tigers", "jaguars"]
+
+# for loop to print out the animal names
+for animal in animals:
+	print(animal)
+
+
+for animal in animals:
+	print(f"\nA {animal} will not make a good pet!")
+
+
+print("\nThese animals are all apart of the big cat family")
+
