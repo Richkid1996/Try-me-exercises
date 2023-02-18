26e052eb043243a8ed063cd751cb196325e86e62
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-02-18 11:04:01 -0600

    Add files via upload

---
+++ 4_10_Slices.py
@@ -0,0 +1,10 @@
+pizzas = ["cheese", "suasage", "peperoni", "pinnaple", "Veggie"]
+
+print("\nThe first three items in the list are:")
+print(pizzas[0:3])
+
+print("\nThe three items from the middle list are:")
+print(pizzas[1:4])
+
+print("\nThe last three items in the list are:")
+print(pizzas[2:])
