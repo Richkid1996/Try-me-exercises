c293cc50edc7b3e31f77e1a0fc894c207c046695
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-03-01 17:27:43 -0600

    Add files via upload

---
+++ 6_1_Persons.py
@@ -0,0 +1,6 @@
+#This is a dictionary that contains Scooters information
+person = {'first_name': 'Scooter', 'last_name': 'Yates', 'age': 26, 'City': 'Pensacola'}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['City'])
