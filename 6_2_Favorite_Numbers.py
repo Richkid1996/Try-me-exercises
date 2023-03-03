a89eaaa47b7dff58f839b8f3fa70996d4d104c5b
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-03-02 19:32:12 -0600

    Add files via upload

---
+++ 6_2_Favorite_Numbers.py
@@ -0,0 +1,10 @@
+favorite_numbers = {"Ken": 67, "Rob": 75, "Kevin": 69}
+
+num = favorite_numbers['Ken']
+print(f"\nKen's favorite number is {num}")
+
+num = favorite_numbers['Rob']
+print(f"\nRob's favorite number is {num}")
+
+num = favorite_numbers['Kevin']
+print(f"\nKevin's favorite number is {num}")
