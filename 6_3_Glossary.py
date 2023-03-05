c278759e3c8ba42ed7745c4b8ae2f2043ab18140
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-03-05 14:55:41 -0600

    Add files via upload

---
+++ 6_3_Glossary.py
@@ -0,0 +1,12 @@
+glossary = {'if statement': 'The simplest kind of if statement has one test and one action',
+'Tuples': 'A list of items that cannot be changed',
+'Range function': 'Makes it easy to generate numbers'}
+
+word ='if statement'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'Tuples'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'Range function'
+print(f"\n{word.title()}: {glossary[word]}")
