a3e7b8761a258c0aad5655ea9b50dde57a094955
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-03-16 21:07:47 -0500

    Add files via upload

---
+++ 7_4.Pizza_Toppings.py
@@ -0,0 +1,11 @@
+prompt = "\nplease enter your pizza toppins here: "
+prompt += "\nEnter 'quit' when your done. "
+
+
+while True:
+	topping = input(prompt)
+
+	if topping != 'quit':
+		print(f"\nI'll add {topping}, to your pizza")
+	else:
+		break
