08a965bf907978d70879b4a6d667584c59b93175
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-03-31 22:05:52 -0500

    Add files via upload

---
+++ 8_5_Cities.py
@@ -0,0 +1,7 @@
+def describe_city(city, country='United States'):
+	"""Display information about the city and the country"""
+	print(f"\n{city} is in the {country}.")
+
+describe_city('Pensacola')
+describe_city('Miami')
+describe_city('Toronto', 'Canada')
