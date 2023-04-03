0a59ed43ff7f7137158b2ad9fdfd51b76699685b
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-04-02 21:11:18 -0500

    Add files via upload

---
+++ 8_7_Album.py
@@ -0,0 +1,17 @@
+def make_album(artist, title):
+	"""Build dictionary describing album titles """
+	album_dict = {
+		'artist': artist.title(),
+		'title' : title.title(),
+		}
+
+	return album_dict
+
+album = make_album('metallica', 'ride the lightning')
+print(album)
+
+album = make_album('beethoven', 'ninth symphony')
+print(album)
+
+album = make_album('willie nelson', 'red-headed stranger')
+print(album)
