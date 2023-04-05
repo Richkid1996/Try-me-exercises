2b47c4d60a279681654f88091b8a58653d93c391
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-04-04 21:29:20 -0500

    Add files via upload

---
+++ 8_9_Messages.py
@@ -0,0 +1,8 @@
+def show_messages(messages):
+	"""Print a simple message """
+	for message in messages:
+		print(message)
+
+
+messages = ['Hope your having a good day', 'Stay bleesed']
+show_messages(messages)
