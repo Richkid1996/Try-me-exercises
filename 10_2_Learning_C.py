45397f70fc4a76ed07c8b60c9df41856cf73857a
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-05-11 17:00:33 -0500

    Add files via upload

---
+++ 10_2_Learning_C.py
@@ -0,0 +1,10 @@
+filename = 'learning_python.txt'
+
+with open(filename) as f:
+	lines = f.readlines()
+
+
+for line in lines:
+	#Get rid of newline, then replace Python with C.
+	line = line.rstrip()
+	print(line.replace('Python', 'C'))
