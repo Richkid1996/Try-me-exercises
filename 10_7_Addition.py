9ec16b7b208efd48ecc7f104a6f3d82b332492ca
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2023-05-23 21:32:56 -0500

    Add files via upload

---
+++ 10_7_Addition.py
@@ -0,0 +1,13 @@
+try:
+	x = input("Give me a number: ")
+	x = int(x)
+
+	y = input("Give me a number: ")
+	y = int(y)
+
+except ValueError:
+	print("Sorry, I really needed a number.")
+
+else:
+	sum = x + y 
+	print(f"The sum of {x} and {y} is {sum}.")
